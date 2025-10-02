d0ae8f8d91162aae1e6b1a8539da2e7f88489cf9
williamzshih <148172235+williamzshih@users.noreply.github.com>
2025-10-02 23:10:49 +0000

    solve two-integer-sum-ii

---
+++ two-pointers/two-integer-sum-ii.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l = 0
+        r = len(numbers) - 1
+
+        while l < r:
+            sum = numbers[l] + numbers[r]
+
+            if sum == target:
+                return [l + 1, r + 1]
+            if sum > target:
+                r -= 1
+            elif sum < target:
+                l += 1
